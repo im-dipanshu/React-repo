a09241d4d37a4c62d748bbef13ae4667fad0b12f
Dipanshu Singh <ds23322seven@gmail.com>
2026-03-21 12:50:49 +0000

    Gallery Project

--- tailwindProject/src/App.jsx
+++ tailwindProject/src/App.jsx
@@ -3,28 +3,22 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import Card from '../components/Card'
 
 function App() {
   const [count, setCount] = useState(0)
+  let myObj={
+    username:"hitesh",
+    age:21
+  }
 
+  let newArr=[1,4]
   return (
-    <>
-      <h1 className='bg-violet-500 text-2xl p-4 rounded-2xl text-white'>Hello From My Side</h1>
+    < >
+      <h1 className='bg-violet-500 text-2xl p-4 rounded-2xl text-white '>Hello From My Side</h1>
 
-      <div class="flex flex-col items-center p-7 rounded-2xl">
-        <div>
-          <img class="size-48 shadow-xl r" alt="" src="/img/cover.png" />
-        </div>
-        <div class="flex">
-          <span>Class Warfare</span>
-          <span>The Anti-Patterns</span>
-          <span class="flex">
-            <span>No. 4</span>
-            <span>·</span>
-            <span>2025</span>
-          </span>
-        </div>
-      </div>
+      <Card username="Dipanshu" btnText="visit me"/>
+      <Card username="chaiaurcode" someObject={newArr} btnText="Search me"/>
     </>
   )
 }
