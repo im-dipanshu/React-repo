77bad8ec875676a0e1733ed20f4d0151d2a588ca
Dipanshu Singh <ds23322seven@gmail.com>
2026-05-27 06:16:43 +0000

    done

--- React-Pages/src/App.jsx
+++ React-Pages/src/App.jsx
@@ -1,5 +1,16 @@
-function App(){
-  return <h1>Hello</h1>
+import React from 'react'
+import Card from './components/card'
+import Logo from './components/logo'
+const App = () => {
+  return (
+    <div>
+      <Logo/>
+      <Card/>
+      <Card/>
+      <Card />
+      <Card/>
+    </div>
+  )
 }
 
 export default App
---
+++ React-Pages/src/components/card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const card = () => {
+  return (
+    <div>
+      <div className='Card'>
+        <h1>Dipanshu Singh</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit atque omnis hic aliquid natus. Veritatis cumque hic blanditiis, et fuga at itaque expedita veniam ipsam, unde dolor iure doloremque assumenda.</p>
+      </div>
+    </div>
+  )
+}
+
+export default card
