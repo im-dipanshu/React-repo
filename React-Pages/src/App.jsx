a33e29c47b20525d436fc9dfa664e9f384433330
Dipanshu Singh <ds23322seven@gmail.com>
2026-05-27 09:01:02 +0000

    Companies Project

--- React-Pages/src/App.jsx
+++ React-Pages/src/App.jsx
@@ -3,12 +3,25 @@ import Card from './components/card'
 import Logo from './components/logo'
 const App = () => {
   return (
+
     <div>
-      <Logo/>
-      <Card/>
-      <Card/>
-      <Card />
-      <Card/>
+      <Logo />
+      <div className='Company-Card'>
+        <div>
+          <Card name='Google' work='Senior UI/UX Designer'image='https://th.bing.com/th/id/OIP.fdZ1QXChVTeaa5YbnGoaZgHaHa?w=183&h=183&c=7&r=0&o=7&dpr=1.2&pid=1.7&rm=3' />
+        </div>
+        <div>
+          <Card name='Netflix' work='Graphic Designer'image='https://th.bing.com/th/id/OIP.XNoGLQtdRIif5uDdIhHtBwHaHa?w=164&h=180&c=7&r=0&o=7&dpr=1.2&pid=1.7&rm=3' />
+        </div>
+        <div>
+          <Card name='Apple' work='Senior Motion Designer'image='https://static.vecteezy.com/system/resources/previews/022/424/614/original/apple-logo-black-isolated-on-white-background-free-vector.jpg'/>
+        </div>
+        <div>
+          <Card name='Amazon' work='Backend Developer'image='https://m.media-amazon.com/images/G/32/highvelocityevents/og_image/logo._CB641320861_.png'/>
+        </div>
+      </div>
+
+
     </div>
   )
 }
--- React-Pages/src/components/card.jsx
+++ React-Pages/src/components/card.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 
-const card = () => {
+const card = (username) => {
+  console.log(username)
   return (
     <div>
       <div className='Card'>
-        <h1>Dipanshu Singh</h1>
+        <img src={username.image}></img>
+        <h1>{username.name}</h1>
+        <h2>{username.work}</h2>
+        <div>
+          <button className='mark'>Part Time</button>
+          <button className='mark'>Senior Level</button>
+        </div>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit atque omnis hic aliquid natus. Veritatis cumque hic blanditiis, et fuga at itaque expedita veniam ipsam, unde dolor iure doloremque assumenda.</p>
+        <button className='find-button'>Find Now</button>
       </div>
     </div>
   )
