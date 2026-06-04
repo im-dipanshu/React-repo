b98c9dac70a6c9ee8784869106b46b5fd5717464
Dipanshu Singh <ds23322seven@gmail.com>
2026-06-04 08:36:25 +0000

    Gallery

--- Gallery/src/App.jsx
+++ Gallery/src/App.jsx
@@ -2,19 +2,25 @@ import axios from 'axios'
 import React, { useState } from 'react'
 
 const App = () => {
-  const[data,SetData]=useState([])
-  const getData=async()=>{
-    const response= await axios.get('https://picsum.photos/v2/list')
-
-    SetData(response.data)
+  const [userData, setUserData] = useState([])
+  const data = async () => {
+    const randompage = Math.floor(Math.random() * 10)
+    const response = await axios.get(`https://picsum.photos/v2/list?page=${randompage}&limit=18`)
+    setUserData(response.data)
+    console.log(response.data)
   }
   return (
     <div>
-      <button onClick={getData}></button>
-       {data.map((Elemen,idx)=>{
+      <button onClick={data} id='click'>Get Data</button>
+      <div id='whole'>
 
-        return <p>{Elemen.author}</p>
-       })}
+        {userData.map((Elem, index) => {
+          return <div id='content'>
+            <img src={Elem.download_url
+            }></img>
+          </div>
+        })}
+      </div>
     </div>
   )
 }
